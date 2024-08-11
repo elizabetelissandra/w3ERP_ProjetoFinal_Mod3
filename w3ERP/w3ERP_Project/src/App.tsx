6ae5adc306e22d43a8dfeff2d920788fcced27ad
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-08-11 11:11:43 -0300

    style(Adicionando página login): :lipstick: Atualizando as cores do projeto com o Material Ui

--- w3ERP/w3ERP_Project/src/App.tsx
+++ w3ERP/w3ERP_Project/src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@mui/material/styles'
 import FormLogin from './pages/Login'
+import { theme } from './context/themeContext'
 
 function App() {
  
 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <FormLogin/>
+    </ThemeProvider>
     </>
   )
 }
---
+++ w3ERP/w3ERP_Project/src/context/themeContext.ts
@@ -0,0 +1,28 @@
+import { createTheme} from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light' || 'dark',
+    primary: {
+      main: '#001C98',
+    },
+    secondary: {
+      main: '#FFC24A',
+    },
+    error: {
+      main: '#FF3333',
+    },
+    warning: {
+      main: '#FFE038',
+    },
+    info: {
+      main: '#004CE8',
+    },
+    success: {
+      main: '#00C247',
+    },
+  },
+  typography: {
+    fontFamily: 'Sora',
+  },
+});
