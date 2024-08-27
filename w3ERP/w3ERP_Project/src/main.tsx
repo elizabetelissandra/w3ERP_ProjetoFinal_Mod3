5327c76a85dfbe4ac24131084615112e4bdf5e51
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-08-27 11:33:00 -0300

    feat(Adicionand Contexto): :sparkles: Adicionando o MenuContext no Main do projeto

--- w3ERP/w3ERP_Project/src/main.tsx
+++ w3ERP/w3ERP_Project/src/main.tsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { GlobalStyles } from './styles/GlobalStyles.ts'
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './context/themeContext.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { GlobalStyles } from "./styles/GlobalStyles.ts";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./context/themeContext.ts";
+import { MenuProvider } from "./context/menuContext.tsx";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalStyles/>
-    <ThemeProvider theme={theme}>
-    <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    <GlobalStyles theme={theme}/>
+    <MenuProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </MenuProvider>
+  </StrictMode>
+);
